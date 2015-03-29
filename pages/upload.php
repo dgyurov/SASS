059e571b1d12c67855bc1aa3fdee7112e0c7344f
Dimitar Gyurov <dgyurov@gmail.com>
2015-03-29 15:42:37 +0200

    Sneaky sneaky.

--- pages/upload.php
+++ pages/upload.php
@@ -1,10 +1,15 @@
 
 <h1>Upload new picture</h1>
-<form action="backend/upload-image.php" method="post" enctype="multipart/form-data">
-	<div class="form-group">
-    	<input type="file" name="fileToUpload" id="fileToUpload">
+<form action="backend/upload-image.php" method="post" enctype="multipart/form-data" onsubmit="return Validate(this);">
+	<div class="panel panel-info">
+	<div class="panel-heading">Only pictures are allowed!</div>
+	<div class="panel-body">
+		<div class="form-group">
+	    	<input type="file" name="fileToUpload" id="fileToUpload">
+	  	</div>
+	  	<button type="submit" value="Upload Image" name="submit" class="btn btn-primary">Upload picture</button>
+	  	</div>
   	</div>
-  	<button type="submit" value="Upload Image" name="submit" class="btn btn-primary">Upload picture</button>
 </form>
 
 
